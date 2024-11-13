f12f4a58ff5821fc02cedbd3bf22971c672538be
wikswannafly <dwitghcahhm@gmail.com>
2024-11-13 20:19:02 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function goToPage2() {
+    document.getElementById('page1').style.display = 'none';
+    document.getElementById('page2').style.display = 'block';
+}
+
+function goToPage1() {
+    document.getElementById('page1').style.display = 'block';
+    document.getElementById('page2').style.display = 'none';
+}
